10c3c1a607d001ba2807c2507d04509e2e0bf8c2
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2024-01-27 21:05:14 -0300

    Autograd update

--- autograd.py
+++ autograd.py
@@ -1,63 +1,164 @@
+from typing import Union, Optional, Callable
+
 import numpy as np
 
+# np.seterr(all='ignore')
+
+Arrayable = Union[int, float, tuple, list, np.ndarray]
+
+def ensure_array(arrayable: Arrayable) -> np.ndarray:
+    if isinstance(arrayable, np.ndarray):
+        return arrayable
+
+    return np.array(arrayable)
+
+Tensorable = Union['Tensor', Arrayable]
+
+def ensure_tensor(tensorable: Tensorable) -> 'Tensor':
+    if isinstance(tensorable, Tensor):
+        return tensorable
+
+    return Tensor(tensorable)
+
 class Tensor:
-    def __init__(self, value, requires_grad=False):
-        if isinstance(value, Tensor):
-            self.data = np.array(value.data)
-            self.grad = value.grad
-        else:
-            self.data = np.array(value)
-            self.grad = None
-
+    def __init__(
+        self, 
+        data: Arrayable, 
+        requires_grad: bool = False, 
+        grad_fn: Optional[Callable[[np.ndarray], None]] = None
+    ):
+        self.data = ensure_array(data)
         self.requires_grad = requires_grad
-
-    def tensor(self, value, requires_grad=False):
-        if isinstance(value, Tensor):
-            return value
+        self.grad_fn = grad_fn
         
-        return Tensor(value, requires_grad=requires_grad)
+        self.grad: Optional[np.ndarray] = None
+        
+        if self.requires_grad:
+            self.zero_grad()
 
     def __repr__(self):
+        ''' Returns a string representation of the tensor '''
+        
         return f'Tensor({self.data}, requires_grad={self.requires_grad})'
 
-    def backward(self, grad=None):
-        if not self.requires_grad:
+    def zero_grad(self):
+        ''' Zeros the gradient of the tensor '''
+        
+        self.grad = np.zeros(self.data.shape)
+    
+    def __add__(self, other):
+        ''' Gets called when using t + other '''
+        
+        other = ensure_tensor(other)
+        
+        data = self.data + other.data
+        requires_grad = self.requires_grad or other.requires_grad
+        
+        def grad_fn(grad: np.ndarray):
+            self.backward(grad)
+            other.backward(grad)
+        
+        return Tensor(data, requires_grad, grad_fn)
+
+    def __radd__(self, other):
+        ''' Gets called when using other + t '''
+        
+        return ensure_tensor(other).__add__(self)
+
+    def __sub__(self, other):
+        ''' Gets called when using t - other '''
+        
+        other = ensure_tensor(other)
+        
+        data = self.data - other.data
+        requires_grad = self.requires_grad or other.requires_grad
+        
+        def grad_fn(grad: np.ndarray):
+            self.backward(grad)
+            other.backward(-grad)
+        
+        return Tensor(data, requires_grad, grad_fn)
+
+    def __rsub__(self, other):
+        ''' Gets called when using other - t '''
+        
+        return ensure_tensor(other).__sub__(self)
+
+    def __mul__(self, other):
+        ''' Gets called when using t * other '''
+        
+        other = ensure_tensor(other)
+        
+        data = self.data * other.data
+        requires_grad = self.requires_grad or other.requires_grad
+        
+        def grad_fn(grad: np.ndarray):
+            self.backward(grad * other.data)
+            other.backward(grad * self.data)
+        
+        return Tensor(data, requires_grad, grad_fn)
+
+    def __rmul__(self, other):
+        ''' Gets called when using other * t '''
+        
+        return ensure_tensor(other).__mul__(self)
+
+    def __pow__(self, other):
+        ''' Gets called when using t ** other '''
+        
+        other = ensure_tensor(other)
+        data = self.data ** other.data
+        
+        requires_grad = self.requires_grad or other.requires_grad
+    
+        def grad_fn(grad: np.ndarray):
+            self.backward(grad * other.data * self.data ** (other.data - 1))
+            other.backward(grad * np.log(self.data) * self.data ** other.data)
+        
+        return Tensor(data, requires_grad, grad_fn)
+
+    def __rpow__(self, other):
+        ''' Gets called when using other ** t '''
+        
+        return ensure_tensor(other).__pow__(self)
+
+    def backward(self, grad: Optional[Arrayable] = None):   
+        if not self.requires_grad or self.grad is None:
             return
 
+        # Initialize gradient if not provided
         if grad is None:
             grad = np.ones(self.data.shape)
         else:
-            grad = np.array(grad)
+            grad = ensure_array(grad)
 
-        # Broadcast gradient if needed
+        # Sum gradient to match data shape
         if self.data.shape != grad.shape:
-            # Sum gradient to match shape of data
-            if self.data.size == 1:
-                
-                grad = grad.sum()
-                
-            # Sum gradient in the axis that doesn't match the shape of data
-            elif self.data.ndim == grad.ndim:
-                
+            keepdims = self.data.ndim == grad.ndim
+            
+            if keepdims:
                 self_shape = np.array(self.data.shape)
-                grad_shape = np.array(grad.shape)
-
-                axis = tuple(np.where(self_shape != grad_shape)[0])
-                
-                grad = grad.sum(axis=axis, keepdims=True)
-
             else:
-                
                 self_shape = np.array((1,) * (grad.ndim - self.data.ndim) + self.data.shape)
-                grad_shape = np.array(grad.shape)
-                
-                axis = tuple(np.where(self_shape != grad_shape)[0])
-                
-                grad = grad.sum(axis=axis)
-                
+            
+            grad_shape = np.array(grad.shape)
+            
+            axis = tuple(np.where(self_shape != grad_shape)[0])
+            
+            grad = ensure_array(grad.sum(axis=axis, keepdims=keepdims))
             grad = grad.reshape(self.data.shape)
             
-        if self.grad is None:
-            self.grad = grad
-        else:
-            self.grad += grad
+        # Accumulate gradient
+        self.grad += grad
+        
+        if self.grad_fn is not None:
+            self.grad_fn(grad)
+            
+y = Tensor([4], requires_grad=False)
+p = Tensor([4], requires_grad=True)
+
+y_p = 0.5 * ((p - y) ** 2)
+
+y_p.backward()
+
+print(p.grad)
