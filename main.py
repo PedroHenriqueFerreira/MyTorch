40b3df5cad2ff529e3f1ee96b61ea600834977d5
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2024-03-16 21:33:12 -0300

    Improving RNN and Tensor

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from mytorch import nn as nn2
 from torch import nn
 import torch
 
-rnn = nn.RNN(5, 6, 2, batch_first=True)
+rnn = nn.RNN(5, 6, 2, batch_first=True, bidirectional=True)
 
 input = torch.randn(3, 5, 5)
-h0 = torch.randn(2, 3, 6, requires_grad=True)
+h0 = torch.randn(4, 3, 6, requires_grad=True)
 output, hn = rnn(input, h0)
 
 print(output)
@@ -18,23 +18,23 @@ output.backward(torch.ones_like(output))
 
 print('-' * 50)
 
-rnn2 = nn2.RNN(5, 6, 2, batch_first=True)
+rnn2 = nn2.RNN(5, 6, 2, batch_first=True, bidirectional=True)
 
-rnn2.cells[0].weight_ih.data = rnn._parameters['weight_ih_l0'].detach().numpy()
-rnn2.cells[0].weight_hh.data = rnn._parameters['weight_hh_l0'].detach().numpy()
-rnn2.cells[0].bias_ih.data = rnn._parameters['bias_ih_l0'].detach().numpy()
-rnn2.cells[0].bias_hh.data = rnn._parameters['bias_hh_l0'].detach().numpy()
+# rnn2.cells[0].weight_ih.data = rnn._parameters['weight_ih_l0'].detach().numpy()
+# rnn2.cells[0].weight_hh.data = rnn._parameters['weight_hh_l0'].detach().numpy()
+# rnn2.cells[0].bias_ih.data = rnn._parameters['bias_ih_l0'].detach().numpy()
+# rnn2.cells[0].bias_hh.data = rnn._parameters['bias_hh_l0'].detach().numpy()
 
-rnn2.cells[1].weight_ih.data = rnn._parameters['weight_ih_l1'].detach().numpy()
-rnn2.cells[1].weight_hh.data = rnn._parameters['weight_hh_l1'].detach().numpy()
-rnn2.cells[1].bias_ih.data = rnn._parameters['bias_ih_l1'].detach().numpy()
-rnn2.cells[1].bias_hh.data = rnn._parameters['bias_hh_l1'].detach().numpy()
+# rnn2.cells[1].weight_ih.data = rnn._parameters['weight_ih_l1'].detach().numpy()
+# rnn2.cells[1].weight_hh.data = rnn._parameters['weight_hh_l1'].detach().numpy()
+# rnn2.cells[1].bias_ih.data = rnn._parameters['bias_ih_l1'].detach().numpy()
+# rnn2.cells[1].bias_hh.data = rnn._parameters['bias_hh_l1'].detach().numpy()
 
 input2 = mytorch.randn(3, 5, 5)
 
 input2.data = input.detach().numpy()
 
-h02 = mytorch.randn(2, 3, 6, requires_grad=True)
+h02 = mytorch.randn(4, 3, 6, requires_grad=True)
 
 h02.data = h0.detach().numpy()
 
--- mytorch/nn/layers.py
+++ mytorch/nn/layers.py
@@ -5,44 +5,46 @@ import mytorch
 from mytorch import Tensor
 from mytorch.nn import ReLU, Tanh
 
+
 class Layer(ABC):
     ''' Abstract class for layers. '''
-    
+
     @abstractmethod
     def named_parameters(self) -> list[tuple[str, Tensor]]:
         ''' Returns the parameters of the layer. '''
-        
+
         pass
-    
+
     @abstractmethod
     def forward(self, x: Tensor, *args) -> Tensor:
         ''' Forward pass. '''
-        
+
         pass
-    
+
     def __call__(self, x: Tensor, *args) -> Tensor:
         return self.forward(x, *args)
-    
+
     def eval(self):
         ''' Sets the layer to evaluation mode. '''
-        
+
         if hasattr(self, 'training'):
             self.training = False
-            
+
         for item in self.__dict__.values():
             if isinstance(item, Layer):
                 item.eval()
-                
+
     def train(self, mode: bool = True):
         ''' Sets the layer to training mode. '''
-        
+
         if hasattr(self, 'training'):
             self.training = mode
-            
+
         for item in self.__dict__.values():
             if isinstance(item, Layer):
                 item.train(mode)
-    
+
+
 class Linear(Layer):
     ''' Linear layer. '''
 
@@ -51,158 +53,196 @@ class Linear(Layer):
         self.out_features = out_features
 
         stdv = 1. / in_features ** 0.5
-        
-        self.weight = mytorch.uniform(-stdv, stdv, (out_features, in_features), mytorch.float32, True)
-        
+
+        self.weight = mytorch.uniform(-stdv, stdv,
+                                      (out_features, in_features), mytorch.float32, True)
+
         if bias:
-            self.bias = mytorch.uniform(-stdv, stdv, (1, out_features), mytorch.float32, True)
+            self.bias = mytorch.uniform(-stdv, stdv,
+                                        (1, out_features), mytorch.float32, True)
         else:
             self.bias = None
-    
+
     def named_parameters(self):
         return [('weight', self.weight), ('bias', self.bias)]
-    
+
     def forward(self, x: Tensor) -> Tensor:
         y = x @ self.weight.T
-        
+
         if self.bias is not None:
             y += self.bias
-            
+
         return y
+
 
 class RNNCell(Layer):
     ''' Elman RNN cell '''
-    
+
     def __init__(self, input_size: int, hidden_size: int, bias: bool = True, nonlinearity: Literal['tanh', 'relu'] = 'tanh'):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bias = bias
         self.nonlinearity = Tanh() if nonlinearity == 'tanh' else ReLU()
-        
+
         stdv = 1 / hidden_size ** 0.5
-        
+
         self.weight_ih = mytorch.uniform(-stdv, stdv, (hidden_size, input_size), mytorch.float32, True)
         self.weight_hh = mytorch.uniform(-stdv, stdv, (hidden_size, hidden_size), mytorch.float32, True)
-        
+
         if bias:
             self.bias_ih = mytorch.uniform(-stdv, stdv, (hidden_size, ), mytorch.float32, True)
             self.bias_hh = mytorch.uniform(-stdv, stdv, (hidden_size, ), mytorch.float32, True)
         else:
             self.bias_ih = None
             self.bias_hh = None
-            
+
     def named_parameters(self):
         return [
-            ('weight_ih', self.weight_ih), 
-            ('weight_hh', self.weight_hh), 
-            ('bias_ih', self.bias_ih), 
+            ('weight_ih', self.weight_ih),
+            ('weight_hh', self.weight_hh),
+            ('bias_ih', self.bias_ih),
             ('bias_hh', self.bias_hh)
         ]
 
     def forward(self, x: Tensor, hx: Optional[Tensor] = None) -> Tensor:
         if hx is None:
             hx = mytorch.zeros((x.shape[0], self.hidden_size), mytorch.float32)
-        
+
         y = x @ self.weight_ih.T + hx @ self.weight_hh.T
-        
+
         if self.bias_ih is not None:
             y += self.bias_ih
-            
+
         if self.bias_hh is not None:
             y += self.bias_hh
-            
+
         return self.nonlinearity(y)
-    
+
+
 class Dropout(Layer):
     ''' Randomly zeroes some of the elements of the input tensor with probability p. '''
 
     def __init__(self, p: float = 0.5):
         self.p = p
-        
+
         self.training = True
         self.scale = 1 / (1 - p)
-    
+
     def named_parameters(self):
         return []
-    
+
     def forward(self, x: Tensor):
         if self.training:
-            mask = mytorch.binomial(1, 1 - self.p, x.shape, mytorch.float32) * self.scale
+            mask = mytorch.binomial(
+                1, 1 - self.p, x.shape, mytorch.float32) * self.scale
         else:
             mask = 1
-            
+
         return x * mask
+
 
 class RNN(Layer):
     ''' Elman RNN '''
-    
+
     def __init__(
-        self, 
-        input_size: int, 
-        hidden_size: int, 
-        num_layers: int, 
-        nonlinearity: Literal['tanh', 'relu'] = 'tanh', 
-        bias: bool = True, 
+        self,
+        input_size: int,
+        hidden_size: int,
+        num_layers: int,
+        nonlinearity: Literal['tanh', 'relu'] = 'tanh',
+        bias: bool = True,
         batch_first: bool = False,
         dropout: float = 0,
-        bidirectional: bool = False # Ignored
+        bidirectional: bool = False  # Ignored
     ):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.bias = bias
-        
+
         self.batch_first = batch_first
         self.bidirectional = bidirectional
-        
-        self.directions = 2 if bidirectional else 1
-        
+
         if dropout > 0:
             self.dropout = Dropout(dropout)
         else:
             self.dropout = None
-        
+
         self.cells = [RNNCell(input_size, hidden_size, bias, nonlinearity)]
-        
+
         for _ in range(num_layers - 1):
             self.cells.append(RNNCell(hidden_size, hidden_size, bias, nonlinearity))
-            
+
     def named_parameters(self):
         parameters: list[tuple[str, Tensor]] = []
-        
+
         for i, cell in enumerate(self.cells):
             for name, parameter in cell.named_parameters():
                 parameters.append((f'{name}_{i}', parameter))
-        
+
         return parameters
-    
+
     def forward(self, x: Tensor, hx: Optional[Tensor] = None) -> Tensor:
         batch_size = x.shape[0] if self.batch_first else x.shape[1]
         sequence_size = x.shape[1] if self.batch_first else x.shape[0]
-        
+
         if hx is None:
-            hx = mytorch.zeros((self.num_layers, batch_size, self.hidden_size), mytorch.float32)
+            directions = 2 if self.bidirectional else 1
+            
+            hx = mytorch.zeros((directions * self.num_layers, batch_size, self.hidden_size), mytorch.float32)
+
+        if self.bidirectional:
+            hidden = [hx[i] for i in range(0, 2 * self.num_layers, 2)]
+            hidden_backward = [hx[i] for i in range(1, 2 * self.num_layers, 2)]
+        else:
+            hidden = [hx[i] for i in range(self.num_layers)]
+            hidden_backward = None
+
+        outs: list[Tensor] = []
         
-        hx_list = [hx[i] for i in range(self.num_layers)]
-        out_list: list[Tensor] = []
-        
+        if self.bidirectional:  
+            outs_reverse: list[Tensor] = []
+        else:
+            outs_reverse = None
+
         for i in range(sequence_size):
-            for j in range(self.num_layers):
-                if j == 0:
+            for layer in range(self.num_layers):
+                if layer == 0:
                     inp = x[:, i] if self.batch_first else x[i]
-                    
-                    hx_list[j] = self.cells[j](inp, hx_list[j])
+
+                    hidden[layer] = self.cells[layer](inp, hidden[layer])
+
+                    if self.bidirectional:
+                        last_inp = x[:, -(i + 1)] if self.batch_first else x[-(i + 1)]
+                        
+                        hidden_backward[layer] = self.cells[layer](last_inp, hidden_backward[layer])
                 else:
-                    hx_list[j] = self.cells[j](hx_list[j - 1], hx_list[j])
+                    hidden[layer] = self.cells[layer](hidden[layer - 1], hidden[layer])
+                    
+                    if self.bidirectional:
+                        hidden_backward[layer] = self.cells[layer](hidden_backward[layer - 1], hidden_backward[layer])
+
+                if layer < self.num_layers - 1 and self.dropout is not None:
+                    hidden[layer] = self.dropout(hidden[layer])
+                    
+                    if self.bidirectional:
+                        hidden_backward[layer] = self.dropout(hidden_backward[layer])
+
+            outs.append(hidden[-1])
             
-                if j < self.num_layers - 1 and self.dropout is not None:
-                    hx_list[j] = self.dropout(hx_list[j])
-            
-            out_list.append(hx_list[-1])
-        
-        out = mytorch.stack(out_list)
+            if self.bidirectional:
+                outs_reverse.append(hidden_backward[-1])
+
+        out = mytorch.stack(outs)
         output = out.swapaxes(0, 1) if self.batch_first else out
-        
-        h_n = mytorch.stack(hx_list)
+
+        if self.bidirectional:
+            out_reverse = mytorch.stack(outs_reverse)
+            output_reverse = out_reverse.swapaxes(0, 1) if self.batch_first else out_reverse
+
+        h_n = mytorch.stack(hidden)
+    
+        if self.bidirectional:
+            output = output.cat([output_reverse], 2)
         
         return output, h_n
